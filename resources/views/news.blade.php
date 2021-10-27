19322046d13096ba18117218f2ec35785ef467f0
Alexey <alexey.dergunov2000@yandex.ru>
2021-10-27 13:08:00 +0400

    Настройка отображения новостей в зависимости от статуса

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -9,10 +9,12 @@
 	<h2 class="article-features">Новости</h2>
 
 	@foreach($data as $el)
-		<div class="alert">
-			<a class="fw-bold h4" href="{{ route('news_page', $el->id) }}">{{ $el->title }}<a>
-			<p class="text-muted">{{$el->preview}}</p>
-			<small>{{$el->created_at}}</small>
-		</div>
+		@if($el->is_active == 1)
+			<div class="alert">
+				<a class="fw-bold h4" href="{{ route('news_page', $el->id) }}">{{ $el->title }}<a>
+				<p class="text-muted">{{$el->preview}}</p>
+				<small>{{$el->created_at}}</small>
+			</div>
+		@endif
 	@endforeach
 @endsection
--- resources/views/news_page.blade.php
+++ resources/views/news_page.blade.php
@@ -1,14 +1,16 @@
-@extends('layouts.v_main')
+@if($data->is_active == 1)
+	@extends('layouts.v_main')
 
-@section('title')
-	{{$data->title}}
-@endsection
+	@section('title')
+		{{$data->title}}
+	@endsection
 
-@section('content')
-	<h2>Новости</h2>
-	<div class="alert">
-		<p>{{ $data->title }}<a>
-		<p class="text-muted">{{$data->content}}</p>
-	</div>
-	@include('includes.comment')
-@endsection
+	@section('content')
+		<h2>Новости</h2>
+		<div class="alert">
+			<p>{{ $data->title }}<a>
+			<p class="text-muted">{{$data->content}}</p>
+		</div>
+		@include('includes.comment')
+	@endsection
+@endif
